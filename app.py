2dd5022ff2115837617454a7f8ca796132afedd2
Shiddarth Sharma <shidd7051@gmail.com>
2026-04-08 20:58:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -214,6 +214,11 @@ def gen_frames():
 def video_feed():
     return Response(gen_frames(),
         mimetype="multipart/x-mixed-replace; boundary=frame")
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 # ---------- MAIN ----------
 if __name__ == "__main__":
